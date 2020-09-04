0cf276ba16ea634863f7aa84a04df1de868da801
Kaskeev <62796577+Kaskeev@users.noreply.github.com>
2020-09-05 05:42:49 +0600

    created first and second page

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('checkPage');
+  checkPageButton.addEventListener('click', function() {
+    alert("hello")
+  })})
